Clever-ops/Dolphin-SFS-RG35xx

Support for real Wiimotes and DolphinBar

Source/Core/DolphinLibretro/Boot.cpp
@@ -114,6 +114,7 @@ bool retro_load_game(const struct retro_game_info* game)
   Config::SetBase(Config::GFX_SHADER_COMPILER_THREADS, 1);
   Config::SetBase(Config::GFX_SHADER_PRECOMPILER_THREADS, 1);
 #endif
+  SConfig::GetInstance().m_WiimoteContinuousScanning = Libretro::Options::bluetoothContinuousScan;
 
   Libretro::Video::Init();
   NOTICE_LOG(VIDEO, "Using GFX backend: %s", SConfig::GetInstance().m_strVideoBackend.c_str());

Source/Core/DolphinLibretro/Options.h
@@ -82,5 +82,6 @@ extern Option<bool> gpuTextureDecoding;
 extern Option<bool> waitForShaders;
 extern Option<bool> forceTextureFiltering;
 extern Option<bool> loadCustomTextures;
+extern Option<bool> bluetoothContinuousScan;
 }  // namespace Options
 }  // namespace Libretro

drizzt/dolphin

(cherry picked from commit c7a8067becc2c46bd19cd2721c01ab8366821509)

Source/Core/DolphinLibretro/Boot.cpp
@@ -90,6 +90,7 @@ bool retro_load_game(const struct retro_game_info* game)
   SConfig::GetInstance().bDPL2Decoder = false;
   SConfig::GetInstance().iLatency = 0;
   SConfig::GetInstance().m_audio_stretch = false;
+  SConfig::GetInstance().m_WiimoteContinuousScanning = Libretro::Options::WiimoteContinuousScanning;
 
   Config::SetBase(Config::SYSCONF_LANGUAGE, (u32)(DiscIO::Language)Libretro::Options::Language);
   Config::SetBase(Config::SYSCONF_WIDESCREEN, Libretro::Options::Widescreen);

Source/Core/DolphinLibretro/Options.h
@@ -113,5 +113,6 @@ extern Option<bool> gpuTextureDecoding;
 extern Option<bool> waitForShaders;
 extern Option<bool> forceTextureFiltering;
 extern Option<bool> loadCustomTextures;
+extern Option<bool> WiimoteContinuousScanning;
 }  // namespace Options
 }  // namespace Libretro